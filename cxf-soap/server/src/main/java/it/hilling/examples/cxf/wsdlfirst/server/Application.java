d6ee87672a5392730debb471ab01e0128e6c9fc2
Gunnar Hilling <gunnar@hilling.de>
2020-05-06 21:36:14 +0200

    cosmetical enhancements

--- cxf-soap/server/src/main/java/it/hilling/examples/cxf/wsdlfirst/server/Application.java
+++ cxf-soap/server/src/main/java/it/hilling/examples/cxf/wsdlfirst/server/Application.java
@@ -1,8 +1,6 @@
 package it.hilling.examples.cxf.wsdlfirst.server;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,19 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application extends RouteBuilder {
 
-    // must have a main method spring-boot can run
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-
-    private static final transient Logger LOG = LoggerFactory.getLogger(Application.class);
-
     @Override
-    public void configure() throws Exception {
-
+    public void configure() {
         from("cxf:bean:customerServiceEndpoint")
-    	//from("cxf:/CustomerServicePort?serviceClass=" + CustomerService.class.getName())
        .to("CustomerServiceProcessor");
     }
 }
